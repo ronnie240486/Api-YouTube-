0e64e9b2bbf62244f894a82901ea3870e7f83fa0
ronnie240486 <ronnie240486@gmail.com>
2025-07-13 17:41:14 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import youtube_busca
+from routers import youtube_real, amazon_real
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-app.include_router(youtube_busca.router, prefix="/youtube")
+app.include_router(youtube_real.router, prefix="/youtube")
+app.include_router(amazon_real.router, prefix="/amazon")
