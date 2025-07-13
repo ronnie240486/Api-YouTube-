f17e4b7328bac1a12d4325c5eda96a39bbde333b
ronnie240486 <ronnie240486@gmail.com>
2025-07-13 19:55:34 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import amazon_scraper, youtube_viralizar
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-app.include_router(amazon_scraper.router, prefix="/amazon")
-app.include_router(youtube_viralizar.router, prefix="/youtube")
-
 @app.get("/")
-def read_root():
-    return {"status": "API ativa: YouTube + Amazon"}
+def root():
+    return {"status": "API Online"}
+
+@app.get("/amazon/testar")
+def testar_variaveis():
+    access_key = os.getenv("AWS_ACCESS_KEY")
+    secret_key = os.getenv("AWS_SECRET_KEY")
+    tag = os.getenv("AMAZON_TAG")
+    endpoint = os.getenv("ENDPOINT")
+    region = os.getenv("REGION")
+    return {
+        "AWS_ACCESS_KEY": access_key,
+        "AWS_SECRET_KEY": "✔️" if secret_key else None,
+        "AMAZON_TAG": tag,
+        "ENDPOINT": endpoint,
+        "REGION": region
+    }
