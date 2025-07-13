312cae0e68f6c1fa3b3aadbb20379e7458fa9fc0
ronnie240486 <ronnie240486@gmail.com>
2025-07-13 18:50:18 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-
 from fastapi import FastAPI
-from routers import youtube_real, amazon_paapi
+from fastapi.middleware.cors import CORSMiddleware
+from routers import youtube_busca, amazon_busca
 
 app = FastAPI()
 
-app.include_router(youtube_real.router, prefix="/youtube")
-app.include_router(amazon_paapi.router, prefix="/amazon")
+# CORS liberado
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Substituir por domínio Netlify se desejar restringir
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(youtube_busca.router, prefix="/youtube")
+app.include_router(amazon_busca.router, prefix="/amazon")
