c09382cac597fffc33d24feff4ef446fa7e582f0
ronnie240486 <ronnie240486@gmail.com>
2025-07-13 12:14:05 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routers import youtube_viralizar
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(youtube_viralizar.router, prefix="/youtube")
