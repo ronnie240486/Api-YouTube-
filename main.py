c880589adb7c64370d8eba7151bfcd9783e140f4
ronnie240486 <ronnie240486@gmail.com>
2025-07-13 19:29:17 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import amazon_real
+from routers import amazon_scraper, youtube_viralizar
 
 app = FastAPI()
 
@@ -12,4 +12,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(amazon_real.router, prefix="/amazon")
+app.include_router(amazon_scraper.router, prefix="/amazon")
+app.include_router(youtube_viralizar.router, prefix="/youtube")
+
+@app.get("/")
+def read_root():
+    return {"status": "API ativa: YouTube + Amazon"}
