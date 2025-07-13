cbf1e0c52873c4aa53e78a464d39bb52dba49165
ronnie240486 <ronnie240486@gmail.com>
2025-07-13 20:23:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
-
 from fastapi import FastAPI
+from routers import amazon_scraper
 from fastapi.middleware.cors import CORSMiddleware
-from routers import amazon_router
-import os
 
 app = FastAPI()
 
-# CORS liberado
+app.include_router(amazon_scraper.router)
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-app.include_router(amazon_router.router, prefix="/amazon")
-
-@app.get("/")
-def root():
-    return {"status": "API ativa"}
