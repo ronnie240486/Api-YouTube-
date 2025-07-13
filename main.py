d1f3012903357ec9c9d51d54b04aaf6858e47b0d
ronnie240486 <ronnie240486@gmail.com>
2025-07-13 13:37:58 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import buscar_videos
 
 app = FastAPI()
+
+# Ativando CORS para permitir requisições do frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ou substitua por ['https://seusite.netlify.app'] para mais segurança
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(buscar_videos.router, prefix="/youtube")
