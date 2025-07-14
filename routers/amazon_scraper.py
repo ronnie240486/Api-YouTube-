8069515e4bf7ed77eaaf738b7fd03ee214f4634a
ronnie240486 <ronnie240486@gmail.com>
2025-07-13 21:05:56 -0300

    Delete routers/amazon_scraper.py

--- routers/amazon_scraper.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from utils.amazon_api import buscar_produtos_reais
-
-router = APIRouter()
-
-@router.get("/amazon/buscar")
-def buscar_produtos(termo: str):
-    return buscar_produtos_reais(termo)
