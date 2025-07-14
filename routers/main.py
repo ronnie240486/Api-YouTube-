55ce530b0940fd787546642f0b264b82f6ceb580
ronnie240486 <ronnie240486@gmail.com>
2025-07-13 21:56:00 -0300

    Add files via upload

--- routers/main.py
+++ routers/main.py
@@ -4,7 +4,3 @@ from routers import amazon_scraper
 app = FastAPI()
 
 app.include_router(amazon_scraper.router, prefix="/amazon")
-
-@app.get("/")
-def root():
-    return {"mensagem": "API Amazon com logging ativado"}
