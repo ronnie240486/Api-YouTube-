1af0e450bfaca9079caebda45d95e7bc9b06e219
ronnie240486 <ronnie240486@gmail.com>
2025-07-13 22:55:23 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
-
 from fastapi import FastAPI
+from routers import youtube_scraper, amazon_scraper
 from fastapi.middleware.cors import CORSMiddleware
-from routers import amazon_scraper, youtube
 
 app = FastAPI()
-
-app.include_router(amazon_scraper.router, prefix="/amazon")
-app.include_router(youtube.router, prefix="/youtube")
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+app.include_router(youtube_scraper.router)
+app.include_router(amazon_scraper.router)
+
 @app.get("/")
 def root():
-    return {"mensagem": "API YouTube + Amazon Online com HMAC"}
+    return {"mensagem": "✅ API YouTube + Amazon Online com HMAC"}
